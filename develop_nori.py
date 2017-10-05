a1de5ee2bca036f8e1bc3d23eb4ae2b2bf43a6e9
Noritamaro <n.ayakoji@gmail.com>
2017-10-05 16:02:07 +0900

    develop

---
+++ develop_nori.py
@@ -0,0 +1,16 @@
+# -*- coding: utf-8 -*-
+
+def fuga():
+    print('fuga')
+
+def hoge():
+    print('huga')
+
+def hikizan(a, b):
+    c = a - b
+    return c
+
+if __name__ == '__main__':
+    fuga()
+    hoge()
+
